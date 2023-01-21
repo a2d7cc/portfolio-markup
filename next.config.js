b8fd03de8f4e1b4066ef60c4daa7aab0e63393c5
a2d7cc <chukrii.alex@gmail.com>
2023-01-21 17:56:01 +0100

    svg import

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          prettier: true,
+          svgo: true,
+          titleProp: true,
+          ref: true,
+        }
+      }]
+    })
+
+    return config
+  },
 }
+
+
+/* 
+          prettier: true,
+          svgo: true,
+          titleProp: true,
+          ref: true,
+ */
 
 module.exports = nextConfig
