eca123f17cea5ea6200ecad30e1c9d01fe58430f
a2d7cc <chukrii.alex@gmail.com>
2023-01-23 19:40:46 +0100

    Header: markup, styles Part1

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
 import Meta from 'providers/meta/Meta'
 import { FC } from 'react'
 
-import Logo from '@/assets/images/logo.svg?inline'
+import logoIcon from '@/assets/images/logo.svg'
 
 import { IHome } from './home.interface'
+import styles from './home.module.scss'
 
 const Home: FC<IHome> = () => {
 	return (
@@ -12,7 +12,34 @@ const Home: FC<IHome> = () => {
 			title="Portfolio"
 			description="Welcome to my personal developer website!"
 		>
-			<h1>Home</h1>
+			<header id={styles.header}>
+				<div className={styles.container}>
+					<div className={styles.inner_header}>
+						<a href="/" className={styles.logo}>
+							<img src={logoIcon.src} alt="Logo icon" />
+						</a>
+						<div className={styles.right_side_header}>
+							<ul className={styles.mnu_top}>
+								<li>
+									<a href="#">About</a>
+								</li>
+								<li>
+									<a href="#">Pricing</a>
+								</li>
+								<li>
+									<a href="#">Contact Us</a>
+								</li>
+								<li>
+									<a href="#">Login</a>
+								</li>
+							</ul>
+							<a href="#" className={styles.btn_simple}>
+								Get Started
+							</a>
+						</div>
+					</div>
+				</div>
+			</header>
 		</Meta>
 	)
 }
